4a409f1ddfb2451e67bda60f49a72020b11768ca
tulirasel420 <tulirasel420@gmail.com>
2026-06-10 13:25:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def run_web_server():
 # --- Configuration Setup ---
 API_TOKEN = '8777471998:AAEJ3LzsWqj8JB15_yzwXOMyS1GHEiGtBbI' 
 ADMIN_ID = 8280240170                                           
-PASSWORD = 'back'
+PASSWORD = 'xnx'
 USER_FILE = 'users.txt'
 
 bot = telebot.TeleBot(API_TOKEN)
@@ -244,7 +244,8 @@ def global_callback_router(call):
         user_data[chat_id]['state'] = 'COLLECTING_BACKTEST'
         user_data[chat_id]['raw_signals'] = []  
         welcome_text = (
-            '<tg-emoji emoji-id="6300758774609092069">🌍</tg-emoji> <b>ORIGINAL BACKTEST MODULE</b>\n\n'
+            '<tg-emoji emoji-id="6276089822490662156">😉</tg-emoji><tg-emoji emoji-id="6278133818901666441">😌</tg-emoji><tg-emoji emoji-id="6276079828101765459">😇</tg-emoji><tg-emoji emoji-id="6275803150603522302">😊</tg-emoji><tg-emoji emoji-id="6275939064843603913">🚀</tg-emoji><tg-emoji emoji-id="6275853229922193308">🅰</tg-emoji><tg-emoji emoji-id="6278459282933419717">🅰</tg-emoji><tg-emoji emoji-id="6276014351325337415">🅰</tg-emoji><tg-emoji emoji-id="6278369556771639533">🅰</tg-emoji>\n'
+            '<tg-emoji emoji-id="6300758774609092069">🌍</tg-emoji> <b> ORIGINAL BACKTEST MODULE</b>\n\n'
             '<tg-emoji emoji-id="6075388783887392362">🚀</tg-emoji> <b>Paste your raw lines below.</b>\n\n'
             '<i>When completely finished, send</i> <b>/done</b> <i>to compile original data.</i>'
         )
@@ -366,7 +367,7 @@ def global_callback_router(call):
                 types.InlineKeyboardButton('🔵 BOTH SIGNALS', callback_data='f_d_3'),
                 types.InlineKeyboardButton('🏠 HOME', callback_data='go_home')
             )
-            bot.edit_message_text(chat_id=chat_id, message_id=call.message.message_id, text="📊 <b>SELECT DIRECTION :</b>", reply_markup=markup, parse_mode='HTML')
+            bot.edit_message_text(chat_id=chat_id, message_id=call.message.message_id, text="<tg-emoji emoji-id=\"6212777095400006386\">💱</tg-emoji> <b>SELECT DIRECTION :</b>", reply_markup=markup, parse_mode='HTML')
         else:
             user_data[chat_id]['action_choice'] = "3"
             user_data[chat_id]['state'] = 'FUTURE_START_TIME'
