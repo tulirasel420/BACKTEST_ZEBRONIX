bae8779d50c535ff63673bb033f47099feeecd9e
tulirasel420 <tulirasel420@gmail.com>
2026-06-28 19:42:47 +0600

    Create pairs.py

---
+++ pairs.py
@@ -0,0 +1,51 @@
+# pairs.py
+
+CURRENCY_PAIRS = {
+    "USDBRL-OTC": "USD/BRL (OTC)",
+    "USDBDT-OTC": "USD/BDT (OTC)",
+    "USDEGP-OTC": "USD/EGP (OTC)",
+    "USDNGN-OTC": "USD/NGN (OTC)",
+    "USDMXN-OTC": "USD/MXN (OTC)",
+    "USDARS-OTC": "USD/ARS (OTC)",
+    "USDDZD-OTC": "USD/DZD (OTC)",
+    "NZDCHF-OTC": "NZD/CHF (OTC)",
+    "USDPHP-OTC": "USD/PHP (OTC)",
+    "USDZAR-OTC": "USD/ZAR (OTC)",
+    "CADCHF-OTC": "CAD/CHF (OTC)",
+    "NZDUSD-OTC": "NZD/USD (OTC)",
+    "AUDNZD-OTC": "AUD/NZD (OTC)",
+    "NZDCAD-OTC": "NZD/CAD (OTC)",
+    "USDCOP-OTC": "USD/COP (OTC)",
+    "USDINR-OTC": "USD/INR (OTC)",
+    "NZDJPY-OTC": "NZD/JPY (OTC)",
+    "USDIDR-OTC": "USD/IDR (OTC)",
+    "USDPKR-OTC": "USD/PKR (OTC)",
+    "EURNZD-OTC": "EUR/NZD (OTC)",
+    "GBPNZD-OTC": "GBP/NZD (OTC)"
+}
+
+COMMODITIES = {
+    "XAGUSD-OTC": "Silver (OTC)",
+    "USCRUDE-OTC": "USCrude (OTC)",
+    "XAUUSD-OTC": "Gold (OTC)"
+}
+
+CRYPTO = {
+    "BNBUSD-OTC": "Binance Coin (OTC)",
+    "TRUUSD-OTC": "Trump (OTC)",
+    "LTCUSD-OTC": "Litecoin (OTC)",
+    "AVAUSD-OTC": "Avalanche (OTC)",
+    "XRPUSD-OTC": "Ripple (OTC)",
+    "DASUSD-OTC": "Dash (OTC)",
+    "ZECUSD-OTC": "Zcash (OTC)",
+    "ETCUSD-OTC": "Ethereum Classic (OTC)",
+    "LINUSD-OTC": "Chainlink (OTC)",
+    "AXSUSD-OTC": "Axie Infinity (OTC)",
+    "ATOUSD-OTC": "Cosmos (OTC)",
+    "DOTUSD-OTC": "Polkadot (OTC)",
+    "SOLUSD-OTC": "Solana (OTC)",
+    "TONUSD-OTC": "Toncoin (OTC)",
+    "ETHUSD-OTC": "Ethereum (OTC)",
+    "BTCUSD-OTC": "Bitcoin (OTC)",
+    "BCHUSD-OTC": "Bitcoin Cash (OTC)"
+}
