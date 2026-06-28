6378eef3f27f6046ee153064d762b90ce9b68795
tulirasel420 <tulirasel420@gmail.com>
2026-06-28 20:07:41 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ API_HASH = "42a88741c882a13d0079758580141c98"  # আপনার API Hash দি
 BOT_TOKEN = "8610661294:AAHeFg1wvPUeBwevgc2G-EMHcceW37IMwP4"  # আপনার Bot Token দিন
 
 # যেই গ্রুপ বা চ্যানেলে জয়েন থাকা বাধ্যতামূলক সেটার ID বা Username
-REQUIRED_CHANNEL = "@irttradingzone"  # অথবা -100xxxxxxxxx ID
+REQUIRED_CHANNEL = "@irttradingzone"  # অথবা -1002658285414 ID
 OWNER_USERNAME = "@RASUU_QXB"
