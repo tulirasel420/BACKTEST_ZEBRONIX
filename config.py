993cc628c4ddc82b8d256aba21c0331b8235c85b
tulirasel420 <tulirasel420@gmail.com>
2026-06-28 19:42:05 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+API_ID = 25635250  # আপনার Telegram API ID দিন (Integer)
+API_HASH = "42a88741c882a13d0079758580141c98"  # আপনার API Hash দিন
+BOT_TOKEN = "8610661294:AAHeFg1wvPUeBwevgc2G-EMHcceW37IMwP4"  # আপনার Bot Token দিন
+
+# যেই গ্রুপ বা চ্যানেলে জয়েন থাকা বাধ্যতামূলক সেটার ID বা Username
+REQUIRED_CHANNEL = "@irttradingzone"  # অথবা -100xxxxxxxxx ID
+OWNER_USERNAME = "@RASUU_QXB"
